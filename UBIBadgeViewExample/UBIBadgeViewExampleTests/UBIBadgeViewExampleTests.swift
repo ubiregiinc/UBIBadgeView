89a9b5d334fd7c27fdf6e54aaf705938b848be1c
Yuki Yasoshima <yukiyasos@gmail.com>
2018-03-23 11:14:57 +0900

    Add example project

---
+++ UBIBadgeViewExample/UBIBadgeViewExampleTests/UBIBadgeViewExampleTests.swift
@@ -0,0 +1,36 @@
+//
+//  UBIBadgeViewExampleTests.swift
+//  UBIBadgeViewExampleTests
+//
+//  Created by Yuki Yasoshima on 2018/03/23.
+//  Copyright © 2018 Ubiregi inc. All rights reserved.
+//
+
+import XCTest
+@testable import UBIBadgeViewExample
+
+class UBIBadgeViewExampleTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
